0609f93352c956b2e2d0c9a4dd01cfc3da0b9e07
vaibhavdangayachvd <vaibhavdangayachvd@gmail.com>
2018-06-03 01:41:42 +0530

    Progress on trees

--- Practise/Data Structures/Trees/Threaded Transversal Insertion and deletion.c
+++ Practise/Data Structures/Trees/Threaded Transversal Insertion and deletion.c
@@ -14,6 +14,8 @@ typedef struct threaded_tree tree_node;
 void hold(void);
 tree_node *insert(tree_node *,int);
 tree_node *in_succ(tree_node*);
+tree_node *in_pre(tree_node*);
+tree_node *del(tree_node*,int);
 void in(tree_node*);
 int main()
 {
@@ -23,7 +25,7 @@ int main()
     jump://Multiple Trials
     system("cls");
     printf("\t\t\t    WELCOME TO TREE DATA-STRUCTURE\n");
-    printf("Choose operation :-\n1 - Insert\n2 - In Transverse\n->");
+    printf("Choose operation :-\n1 - Insert\n2 - Delete\n3 - In Transverse\n   Any Other Key to Exit\n->");
     i = getch();
     system("cls");
     switch(i)
@@ -35,10 +37,17 @@ int main()
         hold();
         break;
     case '2':
+        printf("Enter Key to be deleted : ");
+        scanf("%d",&key);
+        root=del(root,key);
+        hold();
+        break;
+    case '3':
         in(root);
         hold();
         break;
     default:
+        printf("Exit Successful\n");
         return 0;
     }
     goto jump;
@@ -129,4 +138,100 @@ tree_node *in_succ(tree_node*ptr)
             ptr=ptr->lchild;
     }
     return ptr;
+}
+
+tree_node *in_pre(tree_node*ptr)
+{
+    if(ptr->lthread==true)
+        return ptr->lchild;
+    else
+    {
+        ptr=ptr->lchild;
+        while(ptr->rthread==false)
+            ptr=ptr->rchild;
+    }
+    return ptr;
+}
+tree_node *del(tree_node *root,int key)
+{
+    tree_node *ptr,*par,*succ,*par_succ,*child,*p,*s;
+    ptr=root;
+    par=NULL;
+    while(ptr!=NULL)
+    {
+        if(ptr->num==key)
+            break;
+        par=ptr;
+        if(key<ptr->num)
+        {
+            if(ptr->lthread==false)
+                ptr=ptr->lchild;
+            else
+                break;
+        }
+        else
+        {
+            if(ptr->rthread==false)
+                ptr=ptr->rchild;
+            else
+                break;
+        }
+    }
+    if(ptr==par)
+    {
+        printf("Key not found !!!");
+        return root;
+    }
+    if(ptr->lthread==false && ptr->rthread==false)
+    {
+        succ=ptr->rchild;
+        par_succ=ptr;
+        while(succ->lchild!=NULL)
+        {
+            par_succ=succ;
+            succ=succ->lchild;
+        }
+        ptr->num=succ->num;
+        ptr=succ;
+        par=par_succ;
+    }
+    if(ptr->lthread!=false&&ptr->rthread!=false)
+    {
+        if(par==NULL)
+            root=NULL;
+        else if(ptr==par->lchild)
+        {
+            par->lthread=true;
+            par->lchild=ptr->lchild;
+        }
+        else
+        {
+            par->rthread=true;
+            par->rchild=ptr->rchild;
+        }
+    }
+    else
+    {
+        if(ptr->lthread==false)
+            child=ptr->lchild;
+        else
+            child=ptr->rchild;
+        if(par==NULL)
+        {
+            root=child;
+        }
+        else if(ptr==par->lchild)
+            par->lchild=child;
+        else
+            par->rchild=child;
+        p=in_pre(ptr);
+        s=in_succ(ptr);
+        if(ptr->lthread==false)
+            p->rchild=s;
+        else
+            s->lchild=p;
+    }
+    free(ptr);
+    printf("Key Deleted !!\n");
+    return root;
 }
