7f283b5967fb0b222ad740cb6a96446116ba2ce3
vaibhavdangayachvd <vaibhavdangayachvd@gmail.com>
2018-08-28 17:30:49 +0530

    Added Changelog and Documentation And Fixed Some Bugs

--- Personal Projects/Easy Contri by VD/Easy Contry by VD.c
+++ Personal Projects/Easy Contri by VD/Easy Contry by VD.c
@@ -1,7 +1,32 @@
+/**
+    Purpose: Easy Contribution (Linked List)
+    #created 28/08/18 3:10AM
+
+    @author Vaibhav As VD
+    @version 1.0 (28/08/18 3:10AM)
+
+    Change log :-
+    v1 - Initial Release
+       - Inter Group Payments
+       - Club Entry Manager
+       - Added Documentation
+       - Can be compiled on turbo c by just changing type in macro define from 1 to 2
+       Major Bugfix :- Wrong extra amount passed to distribute function.
+       Minor Bugfix :- Reset was not working properly.
+                     - Removed extra parameter in set entry function.
+*/
+//Preprocessor Directives
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
 #include<string.h>
+#define TYPE 1//1 for Code blocks and any other for turbo
+#if TYPE == 1
+#define CLS system("cls")
+#else
+#define CLS clrscr()
+#endif // TYPE
+//Structure of members
 struct details
 {
     char name[20];
@@ -11,27 +36,29 @@ struct details
     struct details *next;
 };
 typedef struct details node;
+//Global Variables
 float entry,per_tot=0,grp_tot=0,bill_amt=0;
-int members;
-void hold_screen(void);
-void create_list(node *head);
-void set_entry_fee(node *head);
-void set_group_activity(node *head);
-node *find_user_node(node *head,char ch[]);
-void calculate(node *head);
-void distri_extra(node *head,float extra,int count);
-void reset(node *head);
+int members = 0;
+//UD Functions
+void hold_screen(void);//Hold Screen
+void create_list(node *head);//Create List
+void set_entry_fee(void);//Set Entry Fee
+void set_group_activity(node *head);//Set Inter group activity
+node *find_user_node(node *head,char ch[]);//Find node from name
+void calculate(node *head);//Calculate Result
+void distri_extra(node *head,float extra,int count);//Recursively Distribute Extra Amount
+void reset(node *head);//Reset Casino
 int main()
 {
     node *head=NULL;
     char option;
     while(1)
     {
-        system("cls");
+        CLS;
         printf("\t\t\tWelcome to Easy Conti by VD\n\n");
         printf("Choose Option :-\n1 - Create List\n2 - Set Entry Fee (optional)\n3 - Inter group payments (optional)\n4 - Calculate\n5 - Reset\n\tBackspace - END\n");
         option = getch();
-        system("cls");
+        CLS;
         switch(option)
         {
         case '1':
@@ -50,7 +77,7 @@ int main()
                     printf("  Wrong Bill Amount !!\n");
                     goto jump;
                 }
-                grp_tot=bill_amt-per_tot;
+                grp_tot=bill_amt-per_tot;//Set Group Total
                 printf("\n\nList Successfully Created !!");
             }
             hold_screen();
@@ -63,7 +90,7 @@ int main()
             else
             {
                 printf("\t\t\tHere You Can Set Entry Fee\n\n");
-                set_entry_fee(head);
+                set_entry_fee();
             }
             hold_screen();
             break;
@@ -93,9 +120,9 @@ int main()
             else
             {
                 reset(head);
+                free(head);
                 head=NULL;
-                entry = 0;
-                per_tot=0;
+                entry = 0,per_tot=0;
                 printf("\n\nReset Successfull !!");
                 hold_screen();
                 break;
@@ -125,6 +152,7 @@ void create_list(node *head)
         head->total=0;
         printf("\n");
         per_tot+=head->per;
+        //If last member set next to null else allot memory
         if(i == members-1)
             head->next=NULL;
         else
@@ -132,7 +160,7 @@ void create_list(node *head)
         head=head->next;
     }
 }
-void set_entry_fee(node *head)
+void set_entry_fee(void)
 {
     printf("Enter entry fee : ");
     jump:
@@ -142,6 +170,7 @@ void set_entry_fee(node *head)
         printf("Wrong Amount !!");
         goto jump;
     }
+    //Treat entry fee as group activity
     grp_tot+=entry;
     printf("\n\t\tEntry Fee Included !!");
 }
@@ -150,6 +179,7 @@ void calculate(node *head)
     node *ptr=head;
     float extra=0.0;
     int count=0;
+    //Add Group amt to grp balance of each
     while(ptr!=NULL)
     {
         ptr->grp+=(grp_tot/members);
@@ -168,18 +198,23 @@ void calculate(node *head)
     {
         while(ptr!=NULL)
         {
+            //If someone spend less than the entry fee
             if(ptr->grp+ptr->per - entry/members <=entry/members)
             {
                 ptr->total=entry/members;
-                extra += entry/members-ptr->grp+ptr->per;
+                //Save its leftover balance
+                extra += entry/members-(ptr->grp+ptr->per - entry/members);
+                //Count number of members whose result is calculated and saved in total field
                 count++;
             }
             ptr=ptr->next;
         }
+        //Distribute Extra Balance in other members who spent more than entry
         distri_extra(head,extra,count);
     }
     ptr = head;
     printf("\t\t\tShare of Each Member :- \n\n");
+    //Print Result
     while(ptr!=NULL)
     {
         printf("%s - Rs. %.2f\n",ptr->name,ptr->total);
@@ -194,18 +229,22 @@ void distri_extra(node *head,float extra,int count)
     while(ptr!=NULL)
     {
         amount = ptr->grp+ptr->per - entry/members - extra/(members-count);
+        //Check if any extra balance left after giving extra
         if(ptr->total==0 && amount<= entry/members)
         {
             ptr->total=entry/members;
-            extra-=extra/(members-count);
-            extra+=entry/members-amount;
+            extra-=extra/(members-count);//Reduce Extra Balance
+            extra+=entry/members-amount;//Add leftover balance to extra
+            //Count number of members whose result is calculated and saved in total field
             count++;
         }
         ptr=ptr->next;
     }
+    //If not extra balance can be accumulated
     if(temp_count==count)
     {
         ptr=head;
+        //Set Result of all by reducing accumulated extra balance from their balance
         while(ptr!=NULL)
         {
             amount = ptr->grp+ptr->per - entry/members - extra/(members-count);
@@ -217,12 +256,13 @@ void distri_extra(node *head,float extra,int count)
         }
         return;
     }
+    //if balance is accumulated try again with updated extra balance and count
     distri_extra(head,extra,count);
 }
 void reset(node *head)
 {
     node *ptr=head;
-    while(head!=NULL)
+    while(head->next != NULL)
     {
         ptr=head;
         head=head->next;
@@ -249,8 +289,10 @@ void set_group_activity(node *head)
     printf("\nEnter Sub Group Amount : ");
     scanf("%f",&total);
     printf("\n");
+    //Sub-Total is added while calculating group total so it is reduced
     grp_tot-=total;
     total/=(float)num;
+    //Adding total to their individual personal grp expenditure
     for(i=0;i<num;++i)
     {
         printf("Enter name of member %d : ",i+1);
